6ff2b87105be207fbe8091148a158cd71ee1afa3
Magnus-Alexander <kodhodetmagnus@gmail.com>
2023-09-04 14:54:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Oppgave 1
+let musikkInstrumenter = ["Trombone", "Eufonium", "Trompet", "Tverrfløyte", "Klarinett", "Bariton", "Tuba", "Piano", "Gitar", "Saxofon"]
+
+let roundedNumber = Math.round(Math.random() * 9)
+console.log(musikkInstrumenter.pop())
+musikkInstrumenter.push("Kornett")
+console.log(musikkInstrumenter[roundedNumber])
+
+// oppgave 1.1
+console.log(`Dette er Index 5: ${musikkInstrumenter[5]} `)
+
+// oppgave 2
